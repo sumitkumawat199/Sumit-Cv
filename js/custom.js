e1aa7aaaea1d1982b6739b6f9089da57b9cb4da1
Sumit Kumawat <kumawatsumit199@gmail.com>
2023-05-23 14:02:14 +0530

    images add

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@ $(document).on("click", 'a[href^="#"]', function (event) {
     {
       scrollTop: $($.attr(this, "href")).offset().top,
     },
-    1500
+    1000
   );
 });
 $(document).ready(function () {
